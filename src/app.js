47d094e3a85eec85a216dce5d7e9b7d71d5c222d
Francisco Tevez <franciscoraultevez@gmail.com>
2025-10-03 18:08:00 -0300

    path fixes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 import CartManager from './cartManager.js';
 import ProductManager from './productManager.js';
-import { engine } from 'express-handlebars';    
 
 const app = express();
 app.use(express.json());
-const cartManager = new CartManager('./carts.json');
-const productManager = new ProductManager('./products.json');
+const cartManager = new CartManager('./src/carts.json');
+const productManager = new ProductManager('./src/products.json');
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', './src/views');
 
 app.get('/', (req, res) => {
     res.render('home')
